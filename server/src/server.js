6c92cbde47aa7f1061d3536d7aca06b9d7107e59
Suleman <suleman.badar.butt@gmail.com>
2026-06-10 06:25:45 +0500

    setup backend and init server

---
+++ server/src/server.js
@@ -0,0 +1,13 @@
+import app from "./app.js"
+
+
+const port=8080;
+
+app.listen(port,  ()=>{
+    console.log(`Server started on port: ${port}`);
+});
+
+console.log("File is running succcessfiully");
+
+
+
