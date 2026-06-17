a5b8789640ec70723defa76893573abac4ba272d
Suleman <suleman.badar.butt@gmail.com>
2026-06-17 06:17:23 +0500

    Created mongoDB connection

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,16 @@
 import app from "./app.js"
-
+import connectDB from "./config/db.js"
+import dns from "node:dns/promises";
+dns.setServers(["1.1.1.1"]);
 
 const port=8080;
 
 app.listen(port,  ()=>{
     console.log(`Server started on port: ${port}`);
+    connectDB();
 });
 
-console.log("File is running succcessfiully");
+// console.log("File is running succcessfiully");
 
 
 
