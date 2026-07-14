59deb1206918099a7fa352a3222c4d4690f348bb
Suleman <suleman.badar.butt@gmail.com>
2026-07-14 07:02:12 +0500

    Migrated my db from mongo to Posgtre( Supabase) and configured all of it

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,29 @@
 import express from "express"
+import prisma from "./libs/prisma.js";
 
-const app = express()
+const app = express();
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
     res.send("Welcome");
+});
+
+app.post("/register", async (req, res) => {
+    const { name, email, password } = req.body;
+    const user = await prisma.user.create({
+        data: {
+            name,
+            email,
+            password
+        }
+    });
+    delete user.password; // Remove password from the response for security reasons
+    res.json(user);
+});
+
+app.get("/users", async (req, res) => {
+    const users = await prisma.user.findMany();
+    users.forEach(user => delete user.password); // Remove password from each user object for security reasons
+    res.json(users);
 });
 
 export default app;
